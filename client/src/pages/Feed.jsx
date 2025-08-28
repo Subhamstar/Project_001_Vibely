cd4597d29d494950e8e2b1ffea9038210b1e1c1c
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-28 12:02:14 +0530

    Feed added

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useEffect,useState } from 'react'
+import { dummyPostsData } from '../assets/assets';
+import Loading from '../components/Loading';
+import StoriesBar from '../components/StoriesBar';
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
+  const [feeds,setfeeds]=useState([]);
+  const [loading,setLoading]=useState(true);
+  const fetchFeeds=async()=>{
+    setfeeds(dummyPostsData)
+    setLoading(false)
+  }
+  useEffect(()=>{
+    fetchFeeds()
+  },[])
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* Stories and Post List  */}
+      <div> 
+        {/* <h1>Stories here</h1> */}
+        <StoriesBar/>
+        <div className='p-4 space-y-6'>
+          List of Post 
+        </div>
+      </div>
+        {/* RightSid Bar  */}
+        <div>
+           <div>
+            <h1>Sponsored</h1>
+           </div>
+           <h1>Recent Messages</h1>
+        </div>
+    </div>
+  ) : <Loading/>
 }
 
 export default Feed
