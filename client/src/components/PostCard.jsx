b04de9394c94a2c4d53d11c13b63018d2e3bc422
Subhamstar <mr.subhamdas999@gmail.com>
2025-09-01 16:40:39 +0530

    postcard added

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import {BadgeCheck} from 'lucide-react'
+import React, { useState } from 'react'
+import {BadgeCheck, Heart, MessageCircle, Share2} from 'lucide-react'
 import moment from 'moment'
+import { dummyUserData } from '../assets/assets';
 const PostCard = ({post}) => {
+    const postWithHasttags=post.content.replace(/(#\w+)/g,'<span class="text-indigo-600">$1</span>');
+    const [likes,setLikes]=useState(post.likes_count);
+    const currentUser=dummyUserData;
+    const handleLike=async()=>{
+
+    }
   return (
     <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl'>
         {/* userInfo  */}
@@ -19,12 +26,28 @@ const PostCard = ({post}) => {
         </div>
 
         {/* Content  */}
-        {post.content && <div className='text-gray-800 text-sm whitespace-pre-line ' dangerouslySetInnerHTML={{__html:post.content}}/>}
+        {post.content && <div className='text-gray-800 text-sm whitespace-pre-line ' dangerouslySetInnerHTML={{__html:postWithHasttags}}/>}
         {/* {image } */}
         <div className='grid grid-cols-2 gap-2'>
             {post.image_urls.map((img,index)=>(
                 <img src={img} key={index} className={`w-full h-48 object-cover rounded-lg ${post.image_urls.length===1 && 'col-span-2 h-auto '}`}alt=''/>
             ))}
+        </div>
+
+        {/* Actions */}
+        <div className='flex items-center gap-4 text=gray-600 text-sm pt-2 border-t border-gray-300'>
+                <div className='flex items-center gap-1'>
+                    <Heart className={`w-4 h-4 cursor-pointer ${likes.includes(currentUser._id)&&'text-red-500 fill-red-500'}`} onClick={handleLike}/>
+                    <span>{likes.length}</span>
+                </div>
+                <div className='flex items-center gap-1'>
+                    <MessageCircle className='w-4 h-4 '/>
+                    <span>{12}</span>
+                </div>
+                <div className='flex items-center gap-1'>
+                    <Share2 className='w-4 h-4 '/>
+                    <span>{7}</span>
+                </div>
         </div>
     </div>
   )
